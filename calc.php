512b273e0deda43fd5fd97fc4b485bd5989d8a8d
Glaubinho03 <85969179+juh22jjuh@users.noreply.github.com>
2022-10-06 11:13:21 -0300

    Update calc.php

--- calc.php
+++ calc.php
@@ -1,7 +1,7 @@
 <?php
 $numero = $_POST['n1'];
 
-if($numero % 10 ==0 && $numero % 5 ==0 && $numero % 2 ==0){
+if($numero % 20 ==0 && $numero % 6 ==0 && $numero % 2 ==0){
     echo "$numero é divisivel por 10, por 5 e por 2";
 }
 else{
